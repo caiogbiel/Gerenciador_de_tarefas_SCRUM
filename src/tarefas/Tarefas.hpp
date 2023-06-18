e5da274d935444e5ff2a657fc3cd061ab10511de
caiogbiel <130727060+caiogbiel@users.noreply.github.com>
2023-06-18 18:17:27 -0300

    Update Tarefas.hpp

--- src/tarefas/Tarefas.hpp
+++ src/tarefas/Tarefas.hpp
@@ -1,1 +1,22 @@
+#ifndef TAREFAS_HPP
+#define TAREFAS_HPP
 
+#include <string>
+
+class Tarefa {
+private:
+    int id;
+    std::string descricao;
+    std::string status;
+    std::string nivelPrioridade;
+
+public:
+    Tarefa(const std::string& titulo, const std::string& descricao);
+    std::string getTitulo() const;
+    std::string getDescricao() const;
+    bool estaConcluida() const;
+    void marcarComoConcluida();
+    void marcarComoEmDesenvolvimento();
+};
+
+#endif
