83ca9767967bd672dcbfcb272313e5a74214e372
caiogbiel <130727060+caiogbiel@users.noreply.github.com>
2023-06-18 18:13:17 -0300

    Update ListaTarefas.hpp

--- src/tarefas/ListaTarefas.hpp
+++ src/tarefas/ListaTarefas.hpp
@@ -1,1 +1,20 @@
+#ifndef LISTATAREFAS_HPP
+#define LISTATAREFAS_HPP
+
+#include <vector>
+#include "Tarefas.hpp"
+
+class ListaTarefas {
+private:
+    std::vector<Tarefa> tarefas;
+    sprint: Sprint;
+
+public:
+    void adicionarTarefa(Tarefa: tarefa);
+    void removerTarefa(Tarefa: tarefa);
+    void iniciarSprint(sprint: Sprint);
+    void finalizarSprint() const;
+};
+
+#endif
 
