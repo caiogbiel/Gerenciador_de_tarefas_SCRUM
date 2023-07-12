623c1f69e58e3ed8ee6e86226aee0c773668dd8d
BKluuis <87882958+BKluuis@users.noreply.github.com>
2023-07-12 17:51:32 -0300

    Documentação de arquivos.hpp

--- include/arquivos.hpp
+++ include/arquivos.hpp
@@ -5,20 +5,22 @@
 #include "./utilities.hpp"
 #include <fstream>
 #include <iostream>
-
+/**
+*  Lê uma lista de itens presentes em um arquivo
+*  @tparam T Tipo a ser lido, requer implementação do operator<<
+*  @param nome_arquivo Caminho do arquivo
+*  @param lista Lista que guardará os itens
+*/
 template <typename T>
 void lerLista(std::string nome_arquivo, adts::Lista<T> &lista);
 
+/**
+*  Guarda uma lista de itens presentes em uma lista
+*  @tparam T Tipo a ser lido, requer implementação do operator>>
+*  @param nome_arquivo Caminho do arquivo
+*  @param lista Lista que contém os itens
+*/
 template <typename T>
 void guardarLista(std::string nome_arquivo, adts::Lista<T> lista);
 
-// void lerScrum(std::string nome_arquivo, adts::Lista<scrum_team> &lista);
-
-// // Leitura de scrum_team e membros apenas
-// void lerMembros(std::string nome_arquivo, adts::Lista<membros> &lista);
-
-// // Leitura de eventos apenas
-// void lerEventos(std::string nome_arquivo, adts::Lista<evento> &lista);
-
-// void guardarMembros(std::string nome_arquivo, adts::Lista<membros> lista);
 #endif
