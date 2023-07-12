3e9e5d30e3601072c4ae4cad12f9b77887e01a25
BKluuis <87882958+BKluuis@users.noreply.github.com>
2023-07-12 17:47:20 -0300

    Documentação de enums.hpp

--- include/enums.hpp
+++ include/enums.hpp
@@ -2,6 +2,7 @@
 #define ENUMS_HPP
 #include <iostream>
 
+/// Contém definição das prioridades das tarefas
 enum prioridade
 {
     baixa,
@@ -10,6 +11,7 @@ enum prioridade
     urgente
 };
 
+/// Contém definição dos tipos de tarefas existentes
 enum eventos_sprint
 {
     planning,
@@ -18,6 +20,7 @@ enum eventos_sprint
     retrospective
 };
 
+/// Contém definição do status das tarefas
 enum status_evento
 {
     analise,
@@ -25,6 +28,7 @@ enum status_evento
     finalizado
 };
 
+/// Contém definição dos niveis de permissão de um usuário
 enum permissao
 {
     product_owner,
@@ -32,9 +36,28 @@ enum permissao
     scrum_master
 };
 
+/**  Transforma uma variáveis do tipo permissão em string
+*    @param[in] p permissão
+*    @return string 
+*/   
 std::string permissao_para_string(permissao p);
+
+/**  Transforma uma variáveis do tipo prioridade em string
+*    @param[in] p Prioridade
+*    @return string 
+*/   
 std::string prioridade_para_string(prioridade p);
+
+/**  Transforma uma variáveis do tipo status_evento em string
+*    @param[in] s Status_evento
+*    @return string 
+*/   
 std::string status_para_string(status_evento s);
+
+/**  Transforma uma variáveis do tipo eventos_sprint em string
+*    @param[in] e Eventos_sprint
+*    @return string 
+*/   
 std::string tipo_para_string(eventos_sprint e);
 
 std::istream &operator>>(std::istream &i, permissao &p);
